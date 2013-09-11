5ee051631ce265e88181ee8aaa5271f4ec816dfb
Martin Schingenga <m.schingenga@planet.nl>
2013-09-11 13:47:43 +0200

    Initial Commit

---
+++ Spectare/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Spectare
+//
+//  Created by Martin Schingenga on 11-09-13.
+//  Copyright (c) 2013 Martin Schingenga. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
